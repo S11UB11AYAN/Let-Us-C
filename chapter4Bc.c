ce9b57186f9112771228b4043f8f62d09cc9e1cf
S11UB11AYAN <shubhayanbagchi68@gmail.com>
2023-06-06 08:39:03 +0530

    question Bc chapter 4

---
+++ chapter4Bc.c
@@ -0,0 +1,21 @@
+/*Two numbers are entered through the keyboard. Write a program to find the value of one number raised to the power of another*/
+
+#include<stdio.h>
+
+#include<math.h>
+
+int main()
+{
+    long num1,num2;
+
+    printf("Enter two numbers:  ");
+
+    scanf("%ld%ld",&num1,&num2);
+
+    long result=pow(num1,num2);
+
+    printf("%ld raise to the power of %ld is %ld\n",num1,num2,result);
+
+    return 0;
+
+}
