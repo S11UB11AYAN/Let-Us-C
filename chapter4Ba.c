a1242baed97e54aa3dcee0da94a826d081948b7a
S11UB11AYAN <shubhayanbagchi68@gmail.com>
2023-06-08 11:42:15 +0530

    small change in 4Ba and Ed solved

--- chapter4Ba.c
+++ chapter4Ba.c
@@ -1,5 +1,5 @@
+/*Write a program to calculate overtime pay of 10 employees. Overtime is paid at the rate of Rs.12.00 per hour for every hour worked above 40 hours. Assume that employees do not work for fractional part of an hour.*/
 #include<stdio.h>
-
 int main()
 {
     int total_pay,hours;
---
+++ chapter4Ed.c
@@ -0,0 +1,26 @@
+/*Write a program to generate all combinations of 1,2 and 3 using for loop*/
+
+#include <stdio.h>
+int main() {
+    int numbers[] = {1, 2, 3};
+    int combinations[27][3];  // Assuming we have 3^3 = 27 combinations
+    int count = 0;
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+            for (int k = 0; k < 3; k++) {
+                combinations[count][0] = numbers[i];
+                combinations[count][1] = numbers[j];
+                combinations[count][2] = numbers[k];
+                count++;
+            }
+        }
+    }
+
+    // Printing the generated combinations
+    for (int i = 0; i < count; i++) {
+        printf("%d %d %d\n", combinations[i][0], combinations[i][1], combinations[i][2]);
+    }
+
+    return 0;
+}
