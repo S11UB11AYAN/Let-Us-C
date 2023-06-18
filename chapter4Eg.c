7d02e17437910a911a5e28cea8bcfd8f9346d5b6
S11UB11AYAN <shubhayanbagchi5@gmail.com>
2023-06-18 09:51:31 +0530

    question g of chapter 4E

---
+++ chapter4Eg.c
@@ -0,0 +1,42 @@
+/*Write a program to produce the following output: 
+A B C D E F G F E D C B A
+A B C D E F   F E D C B A
+A B C D E       E D C B A
+A B C D           D C B A
+A B C               C B A
+A B                   B A
+A                       A
+*/ 
+
+#include<stdio.h>
+int main()
+{
+    for(int i=0;i<7;i++)
+    {
+        for(int j=65;j<=71-i;j++)
+        {
+            printf("%c ",j);
+        }
+        for(int k=1;k<(2*i);k++)
+        {
+            printf("  ");
+        }
+        if(i==0)
+        {
+            for(int j=70;j>=65;j--)
+            {
+                printf("%c ",j);
+            }
+            printf("\n");
+        }
+
+        else
+        {
+            for(int j=70-i+1;j>=65;j--)
+            {
+                printf("%c ",j);
+            }
+            printf("\n");
+        }
+    }
+}
