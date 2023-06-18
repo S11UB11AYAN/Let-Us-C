3f6746561199c399e91cdb8c57dca771a2e27d09
S11UB11AYAN <shubhayanbagchi5@gmail.com>
2023-06-18 11:18:03 +0530

    question h i of chapter 4E

---
+++ chapter4Eh.c
@@ -0,0 +1,36 @@
+/*Write a program to produce the following output: 
+
+                    1
+                2       3
+            4       5       6
+        7       8       9      10
+*/
+
+
+#include<stdio.h>
+int main()
+{
+    int c=1;
+    for(int i=0;i<4;i++)
+    {
+        for(int j=1;j<=3-i;j++)
+        {
+            printf("  ");
+        }
+
+        for(int k=1;k<=(2*i)+1;k++)
+        {
+            if(k%2==0)
+            {
+                printf("  ");
+            }
+            else
+            {
+                printf("%d ",c);
+                c++;
+            }
+            
+        }
+        printf("\n");
+    }
+}
